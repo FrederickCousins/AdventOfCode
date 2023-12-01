8aa8ef6f03cd20ff883f33bb2bbe3d069e77605a
FrederickCousins <fpcousins@outlook.com>
2023-12-01 16:35:08 +0000

    first commit

---
+++ src/day01.py
@@ -0,0 +1,37 @@
+total = 0
+
+nums = "0123456789"
+
+with open('inputs/day01.txt') as f:
+    data = f.readlines()
+
+for line in data:
+    line.strip()
+    ints = [c for c in line if c.isnumeric()]
+    total += int(ints[0]+ints[-1])
+
+print(f"Part 1: {total}")
+
+str2nums = {
+    'one': 'o1e',
+    'two': 't2o',
+    'three': 't3e',
+    'four': 'f4r',
+    'five': 'f5e',
+    'six': 's6x',
+    'seven': 's7n',
+    'eight': 'e8t',
+    'nine': 'n9e',
+    'zero': 'z0o'
+}
+
+total = 0
+for line in data:
+    line.strip()
+    for k, v in str2nums.items():
+        line = line.replace(k,v)
+    ints = [c for c in line if c.isnumeric()]
+    total += int(ints[0]+ints[-1])
+
+print(f"Part 2: {total}")
+
