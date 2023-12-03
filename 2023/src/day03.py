ae2723e759033a1aaa9507b82c23fc355e43577f
FrederickCousins <fpcousins@outlook.com>
2023-12-03 19:26:39 +0000

    Refactor code slightly

--- 2023/src/day03.py
+++ 2023/src/day03.py
@@ -1,54 +1,59 @@
 from math import prod
 
-with open('2023/inputs/day03.txt') as f:
-    data = [[char if char != '.' else '' for char in line.strip()] for line in f]
+with open("2023/inputs/day03.txt") as f:
+    data = [[char if char != "." else "" for char in line.strip()] for line in f]
+
 
 def checkAreaForSymbol(i, j, length) -> bool:
-    '''i, j are the indicies of the last digit of a number which has length''' 
+    """i, j are the indicies of the last digit of a number which has length"""
     l = max(0, j - length)
-    r = min(len(data[i]), j+2)
+    r = min(len(data[i]), j + 2)
 
-    above = data[i-1][l:r] if i > 0 else []
-    below = data[i+1][l:r] if i < len(data) - 1 else []
-   
-    left = data[i][j-length] if j - length >= 0 else ''
-    right = data[i][j+1] if j+1 < len(data[i]) - 1 else ''
-    
+    above = data[i - 1][l:r] if i > 0 else []
+    below = data[i + 1][l:r] if i < len(data) - 1 else []
+
+    left = data[i][j - length] if j - length >= 0 else ""
+    right = data[i][j + 1] if j + 1 < len(data[i]) - 1 else ""
+
     # print(f'tmp: {tmp}, above: {above}, below: {below}, left: {left}, right: {right}\n')
-    
-    if above and any(c and not c.isnumeric() for c in above) or \
-       below and any(c and not c.isnumeric() for c in below) or \
-       left and not left.isnumeric() or \
-       right and not right.isnumeric():
+
+    if (
+        (above and any(c and not c.isnumeric() for c in above))
+        or (below and any(c and not c.isnumeric() for c in below))
+        or (left and not left.isnumeric())
+        or (right and not right.isnumeric())
+    ):
         return True
     else:
         return False
-    
+
+
 res = 0
 for i, row in enumerate(data):
-    tmp = ''
+    tmp = ""
     for j, k in enumerate(row):
-        if k.isnumeric(): # add k to tmp
+        if k.isnumeric():  # add k to tmp
             tmp += k
-                
+
         # There are two cases for clearing tmp
         # 1. We are at the end of current row
         # 2. The next element is not numeric
-        if tmp and (j == len(row) - 1 or not row[j+1].isnumeric()):
+        if tmp and (j == len(row) - 1 or not row[j + 1].isnumeric()):
             if checkAreaForSymbol(i, j, len(tmp)):
                 res += int(tmp)
-            tmp = ''
-            
-print(f'Part 1: {res}')
+            tmp = ""
 
-def extractNumsAroundGear(i,j):
+print(f"Part 1: {res}")
+
+
+def extractNumsAroundGear(i, j):
     nums = []
-    box = [[x for x in r[j-1:j+2]] for r in data[i-1:i+2]]
-    box[1][1] = ''
+    box = [[x for x in r[j - 1 : j + 2]] for r in data[i - 1 : i + 2]]
+    box[1][1] = ""
 
     # for r in box: print(r)
-    
-    for ii, row in enumerate(box,-1):
+
+    for ii, row in enumerate(box, -1):
         if row[1]:
             tmp = row[1]
             jj = 1
@@ -75,19 +80,20 @@ def extractNumsAroundGear(i,j):
                     tmp += data[i + ii][j + 1 + jj]
                     jj += 1
                 nums.append(int(tmp))
-    
+
     return nums
+
 
 res = 0
 for i, row in enumerate(data):
-    tmp = ''
+    tmp = ""
     for j, k in enumerate(row):
-        if k == '*':
-            nums = extractNumsAroundGear(i,j)
-            
+        if k == "*":
+            nums = extractNumsAroundGear(i, j)
+
             # print(nums, '\n')
 
             if len(nums) == 2:
                 res += prod(nums)
 
-print(f'Part 2: {res}')
+print(f"Part 2: {res}")
