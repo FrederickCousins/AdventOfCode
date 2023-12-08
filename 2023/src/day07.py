d997d5b1c2fb30ec5fef9accbf3529cb210fa19a
FrederickCousins <fpcousins@outlook.com>
2023-12-08 11:53:51 +0000

    Add solution for day 7 2023

---
+++ 2023/src/day07.py
@@ -0,0 +1,88 @@
+import aocd
+
+CARD_RANKING = ['2', '3', '4', '5', '6',
+                '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+
+CARD_RANKING2 = ['J', '2', '3', '4', '5', '6',
+                 '7', '8', '9', 'T', 'Q', 'K', 'A']
+
+TYPE_RANKING = ['HC', '1P', '2P', '3K', 'FH', '4K', '5K']
+
+
+def loadInput():
+    data = aocd.get_data(year=2023, day=7).split("\n")
+    hands = [line.split()[0] for line in data]
+    bids = [int(line.split()[1]) for line in data]
+    return hands, bids
+
+
+def classifyHand(hand, joker=False):
+    hand = list(hand)
+    counts = {rank: hand.count(rank)
+              for rank in CARD_RANKING if hand.count(rank) > 0}
+
+    if joker:
+        joker_count = counts.pop("J", 0)
+
+    counts_values = sorted(counts.values(), reverse=True)
+    if not counts_values:
+        counts_values = [0]
+
+    if joker:
+        counts_values[0] += joker_count
+
+    hand_classification = {
+        (5,): '5K',
+        (4, 1): '4K',
+        (3, 2): 'FH',
+        (3, 1, 1): '3K',
+        (2, 2, 1): '2P',
+        (2, 1, 1, 1): '1P'
+    }.get(tuple(counts_values), 'HC')  # type: ignore
+
+    return hand_classification
+
+
+def part1(data):
+    hands, bids = data
+    classifications = [classifyHand(hand) for hand in hands]
+
+    # Combine hands, bids, and classifications into a list of tuples
+    combined_data = list(zip(hands, bids, classifications))
+
+    # Sort by type ranking and card ranking
+    combined_data.sort(key=lambda x: (TYPE_RANKING.index(x[2]), [
+                       CARD_RANKING.index(card) for card in x[0]]))
+
+    # Unpack sorted data
+    hands, bids, classifications = zip(*combined_data)
+
+    # Calculate total
+    total = sum(bid * i for i, bid in enumerate(bids, 1))
+
+    print(f"Part 1: {total}")
+
+
+def part2(data):
+    hands, bids = data
+    classifications = [classifyHand(hand, joker=True) for hand in hands]
+
+    # Combine hands, bids, and classifications into a list of tuples
+    combined_data = list(zip(hands, bids, classifications))
+
+    # Sort by type ranking and card ranking
+    combined_data.sort(key=lambda x: (TYPE_RANKING.index(x[2]), [
+                       CARD_RANKING2.index(card) for card in x[0]]))
+
+    # Unpack sorted data
+    hands, bids, classifications = zip(*combined_data)
+
+    # Calculate total
+    total = sum(bid * i for i, bid in enumerate(bids, 1))
+
+    print(f"Part 2: {total}")
+
+
+INPUT = loadInput()
+part1(INPUT)
+part2(INPUT)
